1207085e9afe15c8950b37d3a55db8e5db4d1942
Patrick Quinlan <ptrckqnln@gmail.com>
2018-06-01 15:44:14 -0400

    Fixes iteritems bug.

---
+++ training_scripts/softmax_downscale.sh
@@ -0,0 +1,23 @@
+python3 src/train_softmax.py \
+--logs_base_dir ~/training/logs/ \
+--models_base_dir ~/training/models/ \
+--pretrained_model ~/Downloads/20180402-114759/20180402-114759/model-20180402-114759.ckpt-275 \
+--data_dir ~/Downloads/train/ \
+--image_size 160 \
+--model_def models.inception_resnet_v1 \
+--optimizer ADAM \
+--learning_rate -1 \
+--max_nrof_epochs 500 \
+--batch_size 90 \
+--keep_probability 0.4 \
+--random_flip \
+--random_downscale \
+--use_fixed_image_standardization \
+--learning_rate_schedule_file data/learning_rate_schedule_classifier_vggface2.txt \
+--weight_decay 5e-4 \
+--embedding_size 512 \
+--lfw_distance_metric 1 \
+--lfw_use_flipped_images \
+--lfw_subtract_mean \
+--validation_set_split_ratio 0.01 \
+--validate_every_n_epochs 5
